4610233ac8d53853802ea6c48b500b26f8be352f
divyansh7705 <divyanshrastogi91@gmail.com>
2024-10-16 15:50:42 +0530

    stack_implementation_using_stl.cpp

---
+++ stack_implementation_using_stl.cpp
@@ -0,0 +1,21 @@
+// Divyansh Rastogi
+// 23070123048
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int>st;
+st.push(30);
+st.push(20);
+st.push(10);
+
+    
+cout<< "Top element of the stack: " <<st.top() <<endl;
+
+    return 0;
+}
+
+/*output:-
+  Top element of the stack: 10
+  */
